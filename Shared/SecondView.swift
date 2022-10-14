13c7dff2e15120303b6e33beb81487d4cbeff5f2
Kanyawatph <kanyawat_ph@hotmail.com>
2022-10-14 15:10:02 +0700

    Create SecondView

---
+++ Shared/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  GitHubDemo
+//
+//  Created by kanyawat phromratsarana on 14/10/2565 BE.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
