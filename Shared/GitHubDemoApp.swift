a4125bb4301cfff62d8d1c4e8e3f41d0fa0f0477
Kanyawatph <kanyawat_ph@hotmail.com>
2022-10-14 14:59:33 +0700

    Initial Commit

---
+++ Shared/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  Shared
+//
+//  Created by kanyawat phromratsarana on 14/10/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
