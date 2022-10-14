017e0acc16a0e2806b259f26279f254496da849b
Kanyawatph <kanyawat_ph@hotmail.com>
2022-10-14 15:43:36 +0700

    change background to blue

--- Shared/MainView.swift
+++ Shared/MainView.swift
@@ -14,7 +14,7 @@ struct MainView: View {
     var body: some View {
         
         ZStack{
-            Color.red.ignoresSafeArea(.all)
+            Color.blue.ignoresSafeArea(.all)
             Text("This is a test")
                        .padding()
         }
